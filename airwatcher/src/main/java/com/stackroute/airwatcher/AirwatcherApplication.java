f5c7ef0957ecd0b0ade3148a47bbcaa4fbec4369
jcnt310 <Git@9999>
2020-03-20 20:44:03 +0530

    spring fse

---
+++ airwatcher/src/main/java/com/stackroute/airwatcher/AirwatcherApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.airwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirwatcherApplication.class, args);
+	}
+
+}
